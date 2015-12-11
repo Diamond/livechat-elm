16de53b9636f0541965c205818123c17ef22bc8d
Brandon Richey <brichey@teladoc.com>
2015-12-11 17:19:30 -0500

    Minor CSS tweaks

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Livechat.Mixfile do
 
   def project do
     [app: :livechat,
-     version: "0.0.2",
+     version: "0.0.3",
      elixir: "~> 1.0",
      elixirc_paths: elixirc_paths(Mix.env),
      compilers: [:phoenix] ++ Mix.compilers,
